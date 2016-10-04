7f6fb8326a47dc293cd8cd6824ce38c357ae92f1
Lyuda Kovalchuk <lyudasmithk@gmail.com>
2016-10-05 00:10:37 +0300

    third  page

---
+++ Cicklum/gulpfile.js
@@ -0,0 +1,22 @@
+/**
+ * Created by lyuda on 27.06.16.
+ */
+var less = require('gulp-less');
+var path = require('path');
+var watch = require('gulp-watch');
+var gulp = require('gulp');
+
+
+gulp.task('less', function () {
+    return gulp.src('./less/stylesheet.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less') ]
+        }))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./less/*.less', ['less']);
+});
+
+gulp.task('default', ['less', 'watch']);
